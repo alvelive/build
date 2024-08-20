55d47933cdc0831531efc0980b1757404563a016
Halil Teyfik Dolmaci <hlltvfk@gmail.com>
2024-08-20 10:46:57 +0300

    chore: verbose log during image name parse

--- parse_image_name.sh
+++ parse_image_name.sh
@@ -42,6 +42,7 @@ main() {
 
   if [ -n "$GITHUB_REPO" ]; then
     if [ "$GITHUB_REPO" = */* ]; then
+      echo "Omitting the owner name from the repository name"
       GITHUB_REPO="$(echo "$GITHUB_REPO" | sed 's|.*/||')"
     fi
 
